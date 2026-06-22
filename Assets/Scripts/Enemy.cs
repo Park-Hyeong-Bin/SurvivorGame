bc24b7013db12c58ce80bf7868cc25303644e950
박형빈 <cavin1999@gmail.com>
2026-06-22 11:58:11 +0900

    feat : 시간 기반 난이도 적 생성 스폰 구성

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,18 +5,27 @@ public class Enemy : MonoBehaviour
 {
     // 적 이동 속도
     public float speed;
+    //현재 체력, 최대체력
+    public float health;
+    public float maxHealth;
+    
+    //몬스터 종류별 애니메이션 컨트롤러 배열
+    public RuntimeAnimatorController[] animCon;
+    // 애니메이션
+    public Animator anim;
     
     // 추적할 대상 물리 컴포넌트 target
     public Rigidbody2D target;
-    
+
     // 적의 생존여부
     bool isLive;
 
     // Enemy 본인 물리 컴포넌트
     Rigidbody2D rigid;
-    
+
     // flipX를 통한 적 좌우 반전용
     SpriteRenderer spriter;
+    
 
 
     void Awake()
@@ -24,8 +33,9 @@ public class Enemy : MonoBehaviour
         // 미리 컴포넌트들을 로드하여 메모리에 캐싱
         rigid = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
-    
+
     // OnEnable: 객체가 활성화 될 때마다 호출
     private void OnEnable()
     {
@@ -38,14 +48,14 @@ public class Enemy : MonoBehaviour
     {
         // 1. 방향 구하기 (목표위치 - 내 위치) -> 플레이어쪽을 바라보는 벡터
         Vector2 dirVec = target.position - rigid.position;
-        
+
         // 2. 이동량 구하기 (방향 * 속도 * 프레임시간)
         // 정규화를 통해 거리랑 상관없이 일정한 속도를 위함
         Vector2 nextVec = dirVec.normalized * (speed * Time.fixedDeltaTime);
-        
+
         // 3. 객체 이동 (현재 위치 + 이동량)
         rigid.MovePosition(rigid.position + nextVec);
-        
+
         // 4. 잔여 속도 제거 플레이어랑 충돌하면, 물리 속도를 0으로
         rigid.linearVelocity = Vector2.zero;
     }
@@ -54,4 +64,15 @@ public class Enemy : MonoBehaviour
     {
         spriter.flipX = target.position.x < rigid.position.x;
     }
+
+
+    //난이도 데이터를 파라미터로 전달받아 적 객체 초기화
+    public void Init(SpawnData data)
+    {
+        anim.runtimeAnimatorController = animCon[data.spriteType];
+        speed = data.moveSpeed;
+        maxHealth = data.health;
+        health = data.health;
+    }
+
 }
